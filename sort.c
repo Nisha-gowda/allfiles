27ac96af1554efbfbab735752f6583d6702744ab
EC2 Default User <ec2-user@ip-172-31-13-49.us-west-1.compute.internal>
2020-12-25 09:45:28 +0000

    added sort.c in workspace12

---
+++ sort.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+ 
+int main ()
+ 
+{
+ 
+   //int i,j,a,n,number[15];
+ 
+     int i,j,a,n=5,number[15];
+ 
+   /*
+ 
+       printf ("\nPlease enter how many numbers you want to sort");
+ 
+       scanf (""%d"", &n);
+ 
+       printf ("\nPlease enter the numbers to be sorted as ascending order");
+ 
+       for (i=0; i<n; ++i)
+ 
+       scanf ("%d",&number[i]);
+ 
+   */
+ 
+   number[0]=4;
+ 
+   number[1]=6;
+ 
+   number[2]=3;
+ 
+   number[3]=7;
+ 
+   number[4]=1;
+ 
+ for (i=0; i<n; ++i)
+ 
